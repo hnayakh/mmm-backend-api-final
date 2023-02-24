4168ed5c50ccff38d94cb69ef0a1e7cb3c958bac
Bibhudutta Das <49719584+bibhuduttadas@users.noreply.github.com>
2023-02-24 10:44:15 +0530

    changed

--- dist/modules/user/entities/user-preference.entity.d.ts
+++ dist/modules/user/entities/user-preference.entity.d.ts
@@ -21,5 +21,5 @@ export declare class UserPreference extends AbstarctEntity {
     smokingHabits: string;
     challenged: string;
     userBasic: UserBasic;
-    static createPreference(minAge: number, maxAge: number, minHeight: number, maxHeight: number, maritalStatus: string, country: string, state: string, city: string, religion: string, caste: string, motherTongue: string, highestEducation: string, occupation: string, maxIncome: string, minIncome: string, dietaryHabits: string, drinkingHabits: string, smokingHabits: string, challenged: string, userBasic: UserBasic): UserPreference;
+    static createPreference(minAge: number, maxAge: number, minHeight: number, maxHeight: number, maritalStatus: any, country: any, state: any, city: any, religion: any, caste: any, motherTongue: any, highestEducation: any, occupation: any, maxIncome: any, minIncome: any, dietaryHabits: any, drinkingHabits: any, smokingHabits: any, challenged: any, userBasic: UserBasic): UserPreference;
 }
