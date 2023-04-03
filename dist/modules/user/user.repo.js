8efdb24b21189d4d43991bba8bfae4a3aecf68e9
Bibhudutta Das <49719584+bibhuduttadas@users.noreply.github.com>
2023-04-03 18:37:52 +0530

    fixed

--- dist/modules/user/user.repo.js
+++ dist/modules/user/user.repo.js
@@ -848,7 +848,7 @@ and  pv.id in (${requiredOnlineUserIds})
     async getBlockedUsers(id) {
         return await this.userBlockRepo.find({
             where: {
-                block_who: id,
+                block_whom: id,
             },
         });
     }
